242b793ac65ddd97e0a86526640a40143c015001
Talish1234 <talishtarik1234@gmail.com>
2024-08-20 02:18:02 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import { useCookies } from 'react-cookie';
+function App() {
+  const [count, setCount] = useState(0);
+  const BASE = "https://test-backend-eight-beta.vercel.app";
+
+  const handleClick = async () => {
+    try {
+      const res = await axios.get(`${BASE}/cookie`,{withCredentials:true});
+     
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleget = async () => {
+    try {
+      const res = await axios.get(`${BASE}/getcookie`,{withCredentials:true});
+      
+     console.log(res.data);
+     if(!res.data)
+    console.log("err");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <>
+      <button onClick={handleClick}>onClick</button>
+      <button onClick={handleget}>onClick</button>
+    </>
+  );
+}
+
+export default App;
